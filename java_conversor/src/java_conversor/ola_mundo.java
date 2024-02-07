4b525cc60642d247207e64dd609ab6838f6be42a
Wallace Benites <148720798+wallaceeBenites@users.noreply.github.com>
2024-02-06 22:18:51 -0300

    Update ola_mundo.java

--- java_conversor/src/java_conversor/ola_mundo.java
+++ java_conversor/src/java_conversor/ola_mundo.java
@@ -4,7 +4,7 @@ public class ola_mundo {
 
 	public static void main(String[] args) {
 		
-		System.out.println("ola mundo asdjasjfasjfjasjfawj");
+		System.out.println("ola mundo novamente");
 	}
 
 }
